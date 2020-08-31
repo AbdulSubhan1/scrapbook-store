f878decbf981951a0b9e9c493cc023bc6cec6ef3
Abdul Subhan <subbidurrani@gmail.com>
2020-08-31 16:08:22 +0500

    initil

---
+++ store.js
@@ -0,0 +1,4 @@
+const scpbook = document.querySelector('.individual-section');
+console.log(document.querySelectorAll('.individual-section'));
+console.log(scpbook.innerText);
+console.log(scpbook.innerHTML);
